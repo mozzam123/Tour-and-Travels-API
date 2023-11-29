adcb361e72f3d9acb33c15d029d74a81fece0fe0
mozzam123 <mozzam607@gmail.com>
2023-11-29 16:17:46 +0530

    added utils folder

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,14 @@
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("./../utils/apiFeatures")
+
 
 // Middleware
-exports.AliasTopTours = async(req, res, next) =>{
+exports.AliasTopTours = async (req, res, next) => {
   req.query.limit = '5'
   req.query.sort = '-ratingsAverage,price'
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
   next();
 }
-
-
-
 
 
 // Get Single Tour
@@ -35,50 +34,10 @@ exports.getTour = async (req, res) => {
 // Get All Tour
 exports.getAlltour = async (req, res) => {
   try {
-    console.log(req.query);
-    // Filtering
-
-    const queryObj = { ...req.query };
-    const excluedFields = ["page", "sort", "limit", "fields"];
-    excluedFields.forEach((el) => delete queryObj[el]);
-
-    // Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // Pagination
-    const page = req.query.page * 1 || 1
-    const limit = req.query.limit * 1 || 50
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit)
-
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("This page does not exists")
-    }
-
 
     // Execute Query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate()
+    const tours = await features.query;
 
     res.status(200).json({
       status: "success",
